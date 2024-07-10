5938d1732fd78e3a658fc3a8aff45ca487ac5025
jth7947 <87174065+jth7947@users.noreply.github.com>
2024-07-10 10:49:15 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,4 @@ app = Flask(__name__)
 @app.route("/")
 
 def home():
-<<<<<<< HEAD
     return "Hello, Flask!"
-=======
-
-    return "Hello, Flask!"
->>>>>>> af51fd07663d05e527676ad9d7ba3bd99a0694e3
